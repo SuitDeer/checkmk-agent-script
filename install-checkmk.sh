b6c4f2aea33878ad0caa03f3634df09887ce29f3
gb991633 <gb991633@fh-muenster.de>
2023-11-01 13:27:21 +0100

    check if dpkg or rpm installed

--- install-checkmk.sh
+++ install-checkmk.sh
@@ -1,34 +1,56 @@
 #!/bin/bash
 
+# parameters needed to be set site-specific.
+SERVER_NAME="v-u-checkmk-p"
+SITE_NAME="cmk"
+API_URL="http://$SERVER_NAME/$SITE_NAME/check_mk/api/1.0"
+USERNAME="automation" 
+PASSWORD="<PASSWORD_OF_THE_AUTOMATION_USER>"
+# End specific parameters
+##### below should not be changed unless you are absolutely sure in what you are doing !
+
+
 # check if running as root in a bash script
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
 
-SERVER_NAME="v-u-checkmk-p"
-SITE_NAME="cmk"
-API_URL="http://$SERVER_NAME/$SITE_NAME/check_mk/api/1.0"
-USERNAME="automation" 
-PASSWORD="<PASSWORD_OF_THE_AUTOMATION_USER>"
+
+# Check if rpm or dpkg package manager is installed
+if command -v dpkg &> /dev/null; then
+  # Download check_mk_agent.deb file from checkmk-server via REST-API
+  curl -H "Accept: application/octet-stream" -H "Authorization: Bearer $USERNAME $PASSWORD" -X GET -H "Content-Type: application/json" -o "/tmp/check_mk_agent.deb" "$API_URL/domain-types/agent/actions/download/invoke?os_type=linux_deb"
+
+  sleep 10
+
+  # Install check_mk_agent
+  dpkg -i /tmp/check_mk_agent.deb
+
+  # Download mk_apt plugin from checkmk server
+  wget http://$SERVER_NAME/$SITE_NAME/check_mk/agents/plugins/mk_apt --no-check-certificate
+  mv -f mk_apt /usr/lib/check_mk_agent/plugins
+  chmod +x /usr/lib/check_mk_agent/plugins/mk_apt
+
+elif command -v rpm &> /dev/null; then
+  # Download check_mk_agent.rpm file from checkmk-server via REST-API
+  curl -H "Accept: application/octet-stream" -H "Authorization: Bearer $USERNAME $PASSWORD" -X GET -H "Content-Type: application/json" -o "/tmp/check_mk_agent.rpm" "$API_URL/domain-types/agent/actions/download/invoke?os_type=linux_rpm"
+
+  sleep 10
+
+  # Install check_mk_agent
+  rpm -i /tmp/check_mk_agent.rpm
+
+else
+  echo "Neither dpkg nor rpm is installed"
+
+fi
+
+
+sleep 10
 
 # Get the host IP address
 HostIP=$(ip -4 route get 8.8.8.8 | awk 'NR==1 {print $7}')
-
-# Download check_mk_agent.deb file from checkmk-server via REST-API
-curl -H "Accept: application/octet-stream" -H "Authorization: Bearer $USERNAME $PASSWORD" -X GET -H "Content-Type: application/json" -o "/tmp/check_mk_agent.deb" "$API_URL/domain-types/agent/actions/download/invoke?os_type=linux_deb"
-
-sleep 10
-
-# Install check_mk_agent
-dpkg -i /tmp/check_mk_agent.deb
-
-# Download mk_apt plugin from checkmk server
-wget http://$SERVER_NAME/$SITE_NAME/check_mk/agents/plugins/mk_apt --no-check-certificate
-mv -f mk_apt /usr/lib/check_mk_agent/plugins
-chmod +x /usr/lib/check_mk_agent/plugins/mk_apt
-
-sleep 10
 
 # Create Host via REST-API
 BODY="{\"folder\": \"/linux_maschinen\", \"host_name\": \"$(hostname | tr '[:upper:]' '[:lower:]')\", \"attributes\": {\"ipaddress\": \"$HostIP\"}}"
@@ -55,7 +77,7 @@ curl -H "Accept: application/json" -H "Authorization: Bearer $USERNAME $PASSWORD
 sleep 60
 
 # Register the Agent
-cmk-agent-ctl register --hostname $(hostname | tr '[:upper:]' '[:lower:]') --server v-u-checkmk-p --site cmk --user automation --password $PASSWORD --trust-cert
+cmk-agent-ctl register --hostname $(hostname | tr '[:upper:]' '[:lower:]') --server $SERVER_NAME --site $SITE_NAME --user $USERNAME --password $PASSWORD --trust-cert
 
 sleep 120
 
--- uninstall-checkmk.sh
+++ uninstall-checkmk.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
+# parameters needed to be set site-specific.
+SERVER_NAME="v-u-checkmk-p"
+SITE_NAME="cmk"
+API_URL="http://$SERVER_NAME/$SITE_NAME/check_mk/api/1.0"
+USERNAME="automation" 
+PASSWORD="<PASSWORD_OF_THE_AUTOMATION_USER>"
+# End specific parameters
+##### below should not be changed unless you are absolutely sure in what you are doing !
+
+
 # check if running as root in a bash script
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
 
-# Uninstall Checkmk Agent
-apt remove check-mk-agent
 
-SERVER_NAME="v-u-checkmk-p"
-SITE_NAME="cmk"
-API_URL="http://$SERVER_NAME/$SITE_NAME/check_mk/api/1.0"
-USERNAME="automation" 
-PASSWORD="<PASSWORD_OF_THE_AUTOMATION_USER>"
+# Check if rpm or dpkg package manager is installed
+if command -v dpkg &> /dev/null; then
+  # Uninstall Checkmk Agent
+  apt remove -y check-mk-agent
 
-# Get the host IP address
-HostIP=$(ip -4 route get 8.8.8.8 | awk 'NR==1 {print $7}')
+elif command -v rpm &> /dev/null; then
+  # Uninstall Checkmk Agent
+  dnf remove -y check-mk-agent
 
-# Download check_mk_agent.deb file from checkmk-server via REST-API
-curl -H "Accept: application/octet-stream" -H "Authorization: Bearer $USERNAME $PASSWORD" -X GET -H "Content-Type: application/json" -o "/tmp/check_mk_agent.deb" "$API_URL/domain-types/agent/actions/download/invoke?os_type=linux_deb"
+else
+  echo "Neither dpkg nor rpm is installed"
 
-sleep 10
+fi
+
 
 # Delete Host via REST-API
 curl -H "Accept: application/json" -H "Authorization: Bearer $USERNAME $PASSWORD" -X DELETE -H "Content-Type: application/json" -d "$BODY" "$API_URL/objects/host_config/$(hostname | tr '[:upper:]' '[:lower:]')"
